d4bad848b459d6106270c64cab6fc77eae786ec2
reithert2424 <reithert2424@uwec.edu>
2022-09-29 22:58:56 -0500

    Initial Mithril Client with Pocketbase and Tailwind

---
+++ src/mithril.js
@@ -0,0 +1,3 @@
+import m from "mithril";
+
+m.render(document.body, "Welcome Troubadour!", {class: "bg-blue-200 rounded-lg"});
---
+++ src/pocketbase.js
@@ -0,0 +1,3 @@
+import PocketBase from '../pocketbase-sdk/pocketbase.es.mjs';
+
+let client = new PocketBase('http://localhost:8090');
